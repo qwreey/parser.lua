69803bb9c813b80d616e8ab403ffa19ab42dce89
qwreey <me@qwreey.moe>
2024-01-07 14:24:42 +0000

    update

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
   --[[lit-meta
     name = "qwreey/parser"
-    version = "2.1.3"
+    version = "2.1.4"
     dependencies = {}
     description = "Parsing support lib"
     tags = { "lua", "parsing" }
@@ -15,34 +15,103 @@ todo
 style handler
 
 should impl
- - mustReachEnd
  - cache
 ]]
 
+local utf8 = utf8 or require "utf8"
+local concat = table.concat
+local insert = table.insert
+local remove = table.remove
+
+-- flatten templates
+local function flatten(comps)
+	local cur,tmp = 1,comps[1]
+	while tmp do
+		if type(tmp) == "table" and rawget(tmp,"__template") then
+			flatten(tmp)
+			remove(comps,cur)
+			for i,v in ipairs(tmp) do
+				insert(comps,cur+i-1,v)
+			end
+		end
+
+		-- next
+		cur = cur + 1
+		tmp = comps[cur]
+	end
+end
+
+local function isCacheable(regex)
+	if regex:sub(1,1) == "^" then return false end
+	return true
+end
+
 local function createParser(comps)
-	local len = #comps/2
 	comps.__index = comps
 	comps.__parser = true
 
-	for _,v in pairs(comps) do
-		if type(v) == "table" and rawget(v,"__parser") and (not v.parent) then
-			v.super = comps
+	-- flatten templates
+	flatten(comps)
+
+	-- Update super for children
+	for key,v in pairs(comps) do
+		if key ~= "__index" and key ~= "super" and type(v) == "table" and rawget(v,"__parser") then
+			if v.super then error("this parser already has super") end
+			if v.super ~= true then
+				v.super = comps
+			end
 		end
 	end
 
-	local function parse(str,start,verbose)
-		local state = setmetatable({},comps)
+	-- unpacking
+	local regexs = {}
+	local funcs = {}
+	local cacheable = {}
+	local len do
+		local index = 1
+		local cur = 1
+		while comps[cur*2-1] do
+			local regex = comps[cur*2-1]
+			local func = comps[cur*2]
+			if func == nil then
+				error("Regular expressions and handler functions are not paired. (length: "..tostring(#comps)..")")
+			end
+			if type(regex) == "table" then -- unpack table
+				for _,childRegex in ipairs(regex) do
+					cacheable[index] = isCacheable(childRegex)
+					regexs[index] = childRegex
+					funcs[index] = func
+					index = index + 1
+				end
+			else
+				cacheable[index] = isCacheable(regex)
+				regexs[index] = regex
+				funcs[index] = func
+				index = index + 1
+			end
+			cur = cur + 1
+		end
+		len = index-1
+	end
+
+	-- main parser function
+	local function parse(self,str,start,verbose)
+		local state = setmetatable({parent = self, __state = true},comps)
+		local findCache = {}
 		local strlen = #str
 		local pos = start or 1
 		local lastpos
 		local init = comps.init
 		local stop = comps.stop
 		local orphans = comps.orphans
+		local tailing = comps.tailing
 		local noEOF = comps.noEOF
+
+		-- Run init function
 		if init then
 			local initPos,initEndup = init(state,str,pos)
-			if initPos then pos = initPos end
-			if initEndup then
+			if initPos then pos = initPos end -- move position if updated
+			if initEndup then -- check endup
 				local result = state
 				if stop then
 					result = stop(state)
@@ -50,8 +119,8 @@ local function createParser(comps)
 				return result,pos
 			end
 		end
-		local findCache = {}
 
+		-- Loop find
 		while true do
 			if verbose then print("LOOP: pos "..pos) end
 
@@ -62,17 +131,8 @@ local function createParser(comps)
 			lastpos = pos
 
 			-- Check buffer exhaust
-			if pos >= strlen then
+			if pos > strlen then
 				if noEOF then -- EOF is not required
-					-- Process orphans
-					if orphans and strlen >= pos then
-						local orphanString = string.sub(str,pos,strlen)
-						if orphanString ~= "" then
-							if verbose then print(" | Push orphans : "..orphanString) end
-							orphans(state,orphanString,true) -- is Tailing
-						end
-					end
-
 					-- Make result
 					local result = state
 					if stop then
@@ -88,13 +148,14 @@ local function createParser(comps)
 			local minFind
 			local minIndex
 			for index=1,len do
-				-- Check old cache by position
 				local find
-				local oldFind = findCache[index] -- use memoization for performance
-				if oldFind and oldFind[1] and oldFind[1] >= pos then
+
+				-- Check old cache by position (use memoization for performance)
+				local oldFind = findCache[index]
+				if cacheable[index] and oldFind and oldFind[1] and oldFind[1] >= pos then
 					find = oldFind
-				elseif (not oldFind) or (oldFind and oldFind[1]) then
-					local regex = comps[index*2-1]
+				elseif (not cacheable[index]) or (not oldFind) or (oldFind[1] and oldFind[1] < pos) then
+					local regex = regexs[index]
 					find = table.pack(string.find(str,regex,pos))
 					findCache[index] = find
 				end
@@ -108,12 +169,12 @@ local function createParser(comps)
 				end
 			end
 
-			if minFind then -- If matchd
-				local func = comps[minIndex*2]
+			if minFind then
+				-- If token found
 				if verbose then
-					print("From "..pos..": "..comps[minIndex*2-1].." ("..minStartAt.."~"..minFind[2]..")")
+					print("From "..pos..": "..regexs[minIndex].." ("..minStartAt.."~"..minFind[2]..")")
 				end
-
+				
 				-- Push orphans
 				if orphans and minStartAt-1 >= pos then
 					local orphanString = string.sub(str,pos,minStartAt-1)
@@ -122,8 +183,9 @@ local function createParser(comps)
 						orphans(state,orphanString,false)
 					end
 				end
-
+				
 				-- Call function and update position
+				local func = funcs[minIndex]
 				local nextPos, endup = func(state,str,pos,table.unpack(minFind,1,minFind.n))
 				pos = nextPos or (minFind[2] + 1)
 				if verbose then
@@ -141,160 +203,351 @@ local function createParser(comps)
 			else
 				-- no token found
 				if verbose then print("No more token") end
-				if noEOF then -- if EOF is not required
-					-- Process orphans
-					if orphans and strlen >= pos then
-						local orphanString = string.sub(str,pos,strlen)
-						if orphanString ~= "" then
-							if verbose then print(" | Push orphans : "..orphanString) end
-							orphans(state,orphanString,true) -- is Tailing
-						end
-					end
-
-					-- Make result
-					local result = state
-					if stop then
-						result = stop(state)
-					end
-					return result,strlen
+				
+				-- if EOF is required
+				if not noEOF then
+					error("No token found")
 				end
-				error("No token found")
+
+				-- Process tailing
+				if tailing and strlen >= pos then
+					local tailingString = string.sub(str,pos,strlen)
+					if tailingString ~= "" then
+						if verbose then print(" | Push tailing : "..tailingString) end
+						tailing(state,tailingString)
+					end
+					pos = strlen + 1
+				end
+
+				-- Make result
+				local result = state
+				if stop then
+					result = stop(state)
+				end
+				return result,pos-1
 			end
 		end
 	end
-	comps.parse = parse
 
 	setmetatable(comps,{
-		__call = function (self,...)
-			return parse(...)
+		__call = function (_,self,...)
+			if type(self) == "table" and rawget(self,"__state") then
+				return parse(self,...)
+			end
+			return parse(nil,self,...)
 		end
 	})
 	return comps
 end
 
-local function commonEscapes(char)
-	if     char == "n"  then return "\n"
-	elseif char == "r"  then return "\r"
-	elseif char == "f"  then return "\f"
-	elseif char == "v"  then return "\v"
-	elseif char == "b"  then return "\b"
-	elseif char == "\\" then return "\\"
-	elseif char == "t"  then return "\t"
-	elseif char == "\"" then return "\""
-	elseif char == "\'" then return "\'"
-	elseif char == "a"  then return "\a"
-	elseif char == "z"  then return "\z"
-	end
+local whitespaces = "%s+"
+local function ignore(_,_,_,_,endAt)
+	return endAt+1
+end
+local function eof(_,_,_,_,endAt)
+	return endAt+1,true
 end
 
-local utf8 = utf8 or require "utf8"
-local function luaEscapes(func)
-	return "\\u{(%x+)}",   function (self,str,pos,startAt,endAt,hex) return func(self,str,pos,startAt,endAt,utf8.char(tonumber(hex,16))) end,
-	       "\\(%d%d?%d?)", function (self,str,pos,startAt,endAt,num) return func(self,str,pos,startAt,endAt,string.char(num)) end,
-	       "\\x(%x%x?)",   function (self,str,pos,startAt,endAt,hex) return func(self,str,pos,startAt,endAt,string.char(tonumber(hex,16))) end,
-	       "\\(.)",        function (self,str,pos,startAt,endAt,char) return func(self,str,pos,startAt,endAt,commonEscapes(char)) end
+local stringEscapes = setmetatable({
+	["n"] = "\n";
+	["r"] = "\r";
+	["f"] = "\f";
+	["v"] = "\v";
+	["b"] = "\b";
+	["\\"] = "\\";
+	["t"] = "\t";
+	["\""] = "\"";
+	["\'"] = "\'";
+	["a"] = "\a";
+	["z"] = "\z";
+},{ __index = function(self,key) return key end })
+
+local function luaStringEscapes(func)
+	return {
+		__template = true,
+		"\\u{(%x+)}",   function (self,str,pos,startAt,endAt,hex) return func(self,str,pos,startAt,endAt,utf8.char(tonumber(hex,16))) end,
+		"\\(%d%d?%d?)", function (self,str,pos,startAt,endAt,num) return func(self,str,pos,startAt,endAt,string.char(num)) end,
+		"\\x(%x%x?)",   function (self,str,pos,startAt,endAt,hex) return func(self,str,pos,startAt,endAt,string.char(tonumber(hex,16))) end,
+		"\\(.)",        function (self,str,pos,startAt,endAt,char) return func(self,str,pos,startAt,endAt,stringEscapes[char]) end,
+	}
 end
 
-function ignore(_,_,_,_,endAt)
-	return endAt
-end
-function eof(_,_,_,_,endAt)
-	return endAt,true
+local function luaNumbers(func)
+	return {
+		__template = true,
+		"(%d*%.?%d+[eE][%+%-]?%d+)", function (self,str,pos,startAt,endAt,expo)
+			return func(self,str,pos,startAt,endAt,tonumber(expo))
+		end,
+		"(0x%x*%.?%x+)", function (self,str,pos,startAt,endAt,hex)
+			return func(self,str,pos,startAt,endAt,tonumber(hex))
+		end,
+		"(0b[10]*%.?[10]+)", function (self,str,pos,startAt,endAt,bin)
+			return func(self,str,pos,startAt,endAt,tonumber(bin))
+		end,
+		"(%d*%.?%d+)", function (self,str,pos,startAt,endAt,num)
+			return func(self,str,pos,startAt,endAt,tonumber(num))
+		end,
+	}
 end
 
-local concat = table.concat
-local insert = table.insert
 local luaStringParser = createParser {
-	super = true;
-	
-	blockQuoteParser = createParser {
+	-- Block
+	blockParser = createParser {
 		"%]%]", eof;
-		init = function (self) self.buffer = {} end;
-		stop = function (self) return concat(self.buffer) end;
-		orphans = function (self,str) insert(self.buffer,str) end;
+		orphans = function (self,str) insert(self.parent.buffer,str) end;
+		tailing = function (self,str) insert(self.parent.buffer,str) end;
 	};
-	"\"", function (self,str,pos,startAt,endAt)
-		if self.open == self.OpenTypes.DoubleQuote then
-			return endAt,true
-		end
-		if self.open then
-			insert(self.buffer,"\"")
-		else
-			self.open = self.OpenTypes.DoubleQuote
-		end
-
-		return endAt
+	"^%[%[", function (self,str,pos,startAt,endAt)
+		local _,parseEndAt = self:blockParser(str,endAt+1)
+		return parseEndAt+1,true
 	end;
-	"'", function (self,str,pos,startAt,endAt)
-		if self.open == self.OpenTypes.SingleQuote then
-			return endAt,true
-		end
-		if self.open then
-			insert(self.buffer,"'")
-		else
-			self.open = self.OpenTypes.SingleQuote
-		end
 
-		return endAt
+	-- Double Quote
+	doubleQuoteParser = createParser {
+		"\"", eof;
+		orphans = function (self,str) insert(self.parent.buffer,str) end;
+		tailing = function (self,str) insert(self.parent.buffer,str) end;
+		luaStringEscapes(function (self,str,pos,startAt,endAt,char)
+			insert(self.parent.buffer,char)
+		end);
+	};
+	"^\"", function (self,str,pos,startAt,endAt)
+		local _,parseEndAt = self:doubleQuoteParser(str,endAt+1)
+		return parseEndAt+1,true
 	end;
-	"%[%[", function (self,str,pos,startAt,endAt)
-		if not self.open then
-			local blockQuoteString,blockQuoteEndAt = self.blockQuoteParser(str,endAt+1)
-			insert(self.buffer,blockQuoteString)
-			return blockQuoteEndAt,true
-		end
 
-		insert(self.buffer,"[[")
-		return endAt
+	-- Single Quote
+	singleQuoteParser = createParser {
+		"\'", eof;
+		orphans = function (self,str) insert(self.parent.buffer,str) end;
+		tailing = function (self,str) insert(self.parent.buffer,str) end;
+		luaStringEscapes(function (self,str,pos,startAt,endAt,char)
+			insert(self.parent.buffer,char)
+		end);
+	};
+	"^'", function (self,str,pos,startAt,endAt)
+		local _,parseEndAt = self:singleQuoteParser(str,endAt+1)
+		return parseEndAt+1,true
 	end;
-	luaEscapes(function (self,str,pos,startAt,endAt,char)
-		insert(self.buffer,char)
-		return endAt
+
+	init = function (self,str,pos) self.buffer = {} end;
+	stop = function (self) return concat(self.buffer) end;
+}
+local function luaStrings(func)
+	return {
+		__template = true,
+		{"\'","\"","%[%["}, function (self,str,pos,startAt,endAt)
+			local result, parseEndAt = luaStringParser(str,startAt)
+			local funcPos,funcStop = func(self,str,pos,startAt,parseEndAt,result)
+			return funcPos or (parseEndAt + 1), funcStop
+		end,
+	}
+end
+
+local function luaValues(func)
+	return {
+		__template = true,
+
+		-- Boolean parsing
+		"true", function (self, str, pos, startAt, endAt)
+			local funcPos,funcStop = func(self,str,pos,startAt,endAt,true)
+			return funcPos or (endAt + 1), funcStop
+		end;
+		"false", function (self, str, pos, startAt, endAt)
+			local funcPos,funcStop = func(self,str,pos,startAt,endAt,false)
+			return funcPos or (endAt + 1), funcStop
+		end;
+
+		-- Nil parsing
+		"nil", function (self, str, pos, startAt, endAt)
+			local funcPos,funcStop = func(self,str,pos,startAt,endAt,nil)
+			return funcPos or (endAt + 1), funcStop
+		end;
+
+		-- Number parsing
+		luaNumbers(function(self, str, pos, startAt, endAt, num)
+			local funcPos,funcStop = func(self,str,pos,startAt,endAt,num)
+			return funcPos or (endAt + 1), funcStop
+		end);
+
+		-- String parsing
+		luaStrings(function(self, str, pos, startAt, endAt, string)
+			local funcPos,funcStop = func(self,str,pos,startAt,endAt,string)
+			return funcPos or (endAt + 1), funcStop
+		end);
+	}
+end
+
+local luaValueParser = createParser {
+	stop = function (self)
+		return self.value
+	end;
+
+	-- common lua values
+	luaValues(function(self, str, pos, startAt, endAt, value)
+		self.value = value
+		return endAt+1,true
 	end);
 
-	orphans = function (self,str)
-		insert(self.buffer,str)
-	end;
-	OpenTypes = {
-		DoubleQuote = 1,
-		SingleQuote = 2,
-		Block = 3,
+	-- table parsing
+	objectParser = createParser {
+		init = function (self,str,pos)
+			self.terminatorRequired = false
+			self.orphanIndex = 1
+		end;
+
+		-- index by string
+		"^%s*([_%w][%w_]*)%s*=%s*", function (self, str, pos, startAt, endAt, key)
+			if self.terminatorRequired then
+				error("token [;,] is require on position "..tostring(startAt))
+			end
+			self.terminatorRequired = true
+			local value, valueEndAt = self.super(str,endAt+1)
+			self.parent.value[key] = value
+			self.terminatorRequired = true
+			return valueEndAt+1
+		end;
+
+		-- index by lua value
+		indexParser = createParser {
+			"^%s*]%s*=%s*", function (self, str, pos, startAt, endAt)
+				return endAt+1,true
+			end;
+		};
+		"^%s*%[", function (self, str, pos, startAt, endAt)
+			if self.terminatorRequired then
+				error("token [;,] is require on position "..tostring(startAt))
+			end
+			self.terminatorRequired = true
+			local key,keyEndAt = self.super(str,endAt+1)
+			local _,indexEndAt = self.indexParser(str,keyEndAt+1)
+			local value, valueEndAt = self.super(str,indexEndAt+1)
+			self.parent.value[key] = value
+			return valueEndAt+1
+		end;
+
+		-- next key
+		"^%s*[;,]", function (self, str, pos, startAt, endAt, key)
+			if not self.terminatorRequired then
+				error("Unexpected token "..str:sub(endAt,endAt).." at position "..tostring(endAt))
+			end
+			self.terminatorRequired = false
+			return endAt+1
+		end;
+
+		-- EOF
+		"^%s*}", function (self, str, pos, startAt, endAt)
+			return endAt+1,true
+		end;
+
+		-- any (array style items)
+		"^%s*().+", function (self, str, pos, startAt, endAt, valueStartAt)
+			if self.terminatorRequired then
+				error("token [;,] is require on position "..tostring(startAt))
+			end
+			self.terminatorRequired = true
+			local index = self.orphanIndex
+			local value, valueEndAt = self.super(str,valueStartAt)
+			self.parent.value[index] = value
+			self.orphanIndex = index+1
+			return valueEndAt+1
+		end;
+		whitespaces, ignore;
 	};
-	openParser = createParser {
-		"^\"", function (self,str,pos,startAt,endAt)
-			self.value = self.super.OpenTypes.DoubleQuote
-			return endAt,true
-		end;
-		"^'", function (self,str,pos,startAt,endAt)
-			self.value = self.super.OpenTypes.SingleQuote
-			return endAt,true
-		end;
-		"^%[%[", function (self,str,pos,startAt,endAt)
-			self.value = self.super.OpenTypes.SingleQuote
-			return endAt,true
-		end;
-		stop = function (self) return self.value end;
-	};
-	init = function (self,str,pos)
-		self.open,pos = self.openParser(str,pos)
-		self.buffer = {}
+	"{", function (self, str, pos, startAt, endAt)
+		-- pass self to object parser
+		self.value = {}
+		local _,parseEndAt = self:objectParser(str,endAt+1)
+		return parseEndAt+1,true
 	end;
-	stop = function (self)
-		return concat(self.buffer)
-	end;
-}
-return {
-	version = "2.1.3";
-	createParser = createParser;
-	whitespaces = "%s+";
-	eof = eof;
-	ignore = ignore;
-	commonEscapes = function (func)
-		return "\\(.)", function(self,str,pos,startAt,endAt,char)
-			return func(self,str,pos,startAt,endAt,commonEscapes(char))
-		end
-	end;
-	luaEscapes = luaEscapes;
-	luaStringParser = luaStringParser;
+	whitespaces, ignore;
 }
 
+-- Allow : instead of =
+-- Allow no ; or , (eg: { true true true { true } true } )
+local luaNonstrictValueParser = createParser {
+	stop = function (self)
+		return self.value
+	end;
+
+	-- common lua values
+	luaValues(function(self, str, pos, startAt, endAt, value)
+		self.value = value
+		return endAt+1,true
+	end);
+
+	-- table parsing
+	objectParser = createParser {
+		init = function (self,str,pos)
+			self.orphanIndex = 1
+		end;
+
+		-- index by string
+		"^%s*([_%w][%w_]*)%s*[=:]%s*", function (self, str, pos, startAt, endAt, key)
+			local value, valueEndAt = self.super(str,endAt+1)
+			self.parent.value[key] = value
+			return valueEndAt+1
+		end;
+
+		-- index by lua value
+		indexParser = createParser {
+			"^%s*]%s*[=:]%s*", function (self, str, pos, startAt, endAt)
+				return endAt+1,true
+			end;
+		};
+		"^%s*%[", function (self, str, pos, startAt, endAt, key)
+			local key,keyEndAt = self.super(str,endAt+1)
+			local _,indexEndAt = self.indexParser(str,keyEndAt+1)
+			local value, valueEndAt = self.super(str,indexEndAt+1)
+			self.parent.value[key] = value
+			return valueEndAt+1
+		end;
+
+		-- next key
+		"^%s*[;,]", function (self, str, pos, startAt, endAt, key)
+			return endAt+1
+		end;
+
+		-- EOF
+		"^%s*}", function (self, str, pos, startAt, endAt)
+			return endAt+1,true
+		end;
+
+		-- any (array style items)
+		"^%s*().+", function (self, str, pos, startAt, endAt, valueStartAt)
+			local index = self.orphanIndex
+			local value, valueEndAt = self.super(str,valueStartAt)
+			self.parent.value[index] = value
+			self.orphanIndex = index+1
+			return valueEndAt+1
+		end;
+		whitespaces, ignore;
+	};
+	"{", function (self, str, pos, startAt, endAt)
+		-- pass self to object parser
+		self.value = {}
+		local _,parseEndAt = self:objectParser(str,endAt+1)
+		return parseEndAt+1,true
+	end;
+	whitespaces, ignore;
+}
+
+return {
+	version = "2.1.4";
+	createParser = createParser;
+
+	-- parsers
+	luaNonstrictValueParser = luaNonstrictValueParser;
+	luaValueParser = luaValueParser;
+	luaStringEscapes = luaStringEscapes;
+	luaStringParser = luaStringParser;
+
+	-- utility
+	ignore = ignore;
+	whitespaces = whitespaces;
+	eof = eof;
+
+	-- templates
+	luaNumbers = luaNumbers;
+	luaStrings = luaStrings;
+}
--- test/noEOF.lua
+++ test/noEOF.lua
@@ -1,49 +1,25 @@
 local parser = require "../init.lua"
 local print = p
 
--- Execute all regular expressions, and execute the function of the closest match.
--- If they exist in exactly the same place, execute the function of the one further up in the code (preceded by the index).
 local simpleParser = parser.createParser {
-	-- Regex, and handle function
 	"%${([%w_]+)}", function(self,str,pos,startAt,endAt,match1) -- self(context), original string, and string.find results
 		table.insert(self.inst,{t="val", v=match1})
-		-- Handler function can return next search start position and whether parsing has ended
-		-- If function returns nothing, use endAt+1 as next search position
 	end;
-
-	-- Handle characters not found by matching
 	orphans = function (self,orphan)
-		-- Raise an error if a character not found by matching shouldn't exist
-		-- Or handle orphan string
 		table.insert(self.inst,{t="str", v=orphan})
 	end;
-
-	-- Execute when parsing started
-	init = function (self,str,pos)
-		-- print(pos) -- 1, starting position
-		self.inst = {}
-		-- init can return starting position. If return nothing, use pos as staring position
+	tailing = function (self,orphan)
+		table.insert(self.inst,{t="str", v=orphan})
 	end;
-
-	-- Execute when parsing ended. it should return result (If not, use self as result)
+	init = function (self,str,pos)
+		self.inst = {}
+	end;
 	stop = function (self)
 		return self.inst
 	end;
-
-	-- This means that there may be no terminator (EOF), and parsing may proceed to the end of the string.
-	-- If this value is not present, an error will be thrown if no terminator is found before the end of the string is reached
 	noEOF = true;
-
-	-- Whether use result caching. Only use if result won't change every time you parse same value
-	-- This can be useful if you'll be using the same value over and over again
 	cache = true;
 }
--- simpleParser will return result and ending position
-local result, endPos = simpleParser("test${value1}test")
-print(result, endPos)
--- { {t="str", v="test"}, {t="val", v="value1"}, {t="str", v="test"} }
--- 17
-print(simpleParser("test${value1}test") == result) -- true (cached)
 
 local function f(format)
 	local parsed = simpleParser(format)
